6e91ef9ffdd6ac22d43b97f6fb596fca8dc84a58
Perumal <149862850+19BCS046@users.noreply.github.com>
2024-03-19 15:57:45 +0530

    Delete App.js

--- App.js
+++
@@ -1,5 +0,0 @@
-const main=React.createElement("div",{id:"grandParent"},[
-React.createElement("div",{id:"parent"},
-React.createElement("h1",{id:"child"},"I am the child"),React.createElement("h2",{id:"second_child"},"I am the second Child "))]);
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(main);
